5a5e1fc56c35c21a602974f8a55de60a84dd634d
bara-934 <160777859+bara-934@users.noreply.github.com>
2024-04-24 11:02:08 +0200

    fix the logical bug causing user to always win or tie with scissors in the playRound function

--- rockPaperScissors/script.js
+++ rockPaperScissors/script.js
@@ -3,7 +3,7 @@ console.log("enter 'playGame()' below :")
 
 let humanScore = 0, computerScore = 0;
 
-let getComputerChoice = function(){
+function getComputerChoice(){
     let computerChoice = Math.floor(Math.random()*3);
     computerChoice = computerChoice === 0 ? "rock" :
     computerChoice === 1 ? "paper" :
@@ -13,10 +13,10 @@ let getComputerChoice = function(){
 
 
 function playRound(humanChoice, computerChoice){
-    let humanWins = humanChoice === computerChoice ? console.log("Tie!") :
-    (humanChoice === "paper" && computerChoice === "rock") ? true :
-    humanChoice > computerChoice ? false : true;
-    return humanWins;
+    let result = humanChoice === computerChoice ? 'noWinner' :
+    (humanChoice === "paper" && computerChoice === "rock") ? 'humanWins' :
+    humanChoice < computerChoice ? 'computerWins' : 'humanWins';
+    return result;
     
 }
 
@@ -25,17 +25,20 @@ function playGame(rounds = parseInt(prompt("First to ?: (10 max) "))){
         console.log("to much rounds required. Choose a number beetween 1 and 10");
         playGame();
     }
+    humanScore = 0;
+    computerScore = 0;
     while( humanScore < rounds && computerScore < rounds){
         let humanChoice = prompt("rock, paper, or scissors? ").toLowerCase();
+        
         while( humanChoice!== 'rock' && humanChoice!== 'paper'&& humanChoice!== 'scissors'){
             humanChoice = prompt("Impossibke choice! Only Rock, paper and scissors are accepted. Try again or type 'Stop'").toLowerCase();
             if (humanChoice === 'stop' ) return;
         }
+
         let computerChoice = getComputerChoice()
-        if(playRound(humanChoice,computerChoice)) {console.log(`+1 for you! The score is ${++humanScore} for you and ${computerScore} for the Computer!`)
-        } else if (playRound(humanChoice,computerChoice) === false) {
-        console.log(`The computer take this round! The score is now ${humanScore} for you and ${++computerScore} for the Computer!`)
-        } else console.log(`The score is still ${humanScore} for you and ${computerScore} for the Computer`)
+        if(playRound(humanChoice,computerChoice) === 'humanWins') {console.log(`+1 for you! The score is ${++humanScore} for you and ${computerScore} for the Computer!`)
+        } else if (playRound(humanChoice,computerChoice) === 'computerWins') {console.log(`The computer take this round! The score is now ${humanScore} for you and ${++computerScore} for the Computer!`)
+        } else console.log(`It's a tie! The score is still ${humanScore} for you and ${computerScore} for the Computer`)
     }
 
     humanScore === rounds ? alert(`Congrats! You are the winner! SCORE: ${humanScore} - ${computerScore}`): alert('GAME OVER')||console.log(`You lost ${computerScore} to ${humanScore}`);
