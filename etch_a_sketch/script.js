c8f75142b7f09c3b15c4834ac8ccb7602ff11684
bara-934 <160777859+bara-934@users.noreply.github.com>
2024-05-03 16:51:36 +0200

    add color randomizer and opacity effect make the prompt button bigger fix the rgb digits recuperarion issue by adding new attributes to store the rgb datas

--- etch_a_sketch/script.js
+++ etch_a_sketch/script.js
@@ -33,20 +33,20 @@ function createGrid(dimensions)
         display:flex;`;
 
         for(let i = 0; i < dimensions.width; i++)
-            {
-                const square = document.createElement("div");
-                row.appendChild(square);
-                square.style.cssText = 
-                `border:1px solid darkblue;
-                 flex:1 0 auto`;
-                 square.addEventListener('mouseover',()=> {
-                    square.style.backgroundColor = 'darkblue';
-                 })
-                 square.addEventListener('mouseout',()=> {
-                    square.style.backgroundColor = 'transparent';
-                 })
-    
-            }        
+        {
+            const square = document.createElement("div");
+            row.appendChild(square);
+            square.style.cssText = 
+            `border:1px solid darkblue;
+            flex:1 0 auto`;
+            square.addEventListener('mouseover',(e)=> {
+            squareColor(e.target);
+            });
+            // square.addEventListener('mouseout',()=> {
+            // square.style.backgroundColor = 'transparent';
+            // })
+
+        }        
         newContainer.appendChild(row);
     }
     document.body.replaceChild(newContainer,container)
@@ -57,15 +57,29 @@ function squareColor(square){
     if(square.className !== 'passed')
     {
         square.className = 'passed';
-        square.style.backgroundColor = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`
+        let rInitial = getRandomInt(255);
+        let gInitial = getRandomInt(255);
+        let bInitial = getRandomInt(255);
+        square.setAttribute("data-rInitial",`${rInitial}`);
+        square.setAttribute("data-gInitial",`${gInitial}`);
+        square.setAttribute("data-bInitial",`${bInitial}`);
+        square.setAttribute("data-r",`${rInitial}`);
+        square.setAttribute("data-g",`${gInitial}`);
+        square.setAttribute("data-b",`${bInitial}`);
+        
+        square.style.backgroundColor = `rgb(${rInitial}}, ${gInitial}}, ${bInitial}})`
     } 
     else 
     {
-        let prevBackground = square.style.backgroundColor;
-        let r = Number(prevBackground.slice(4,7)) * 0.85;
-        let g = Number(prevBackground.slice(8,11)) * 0.85;
-        let b = Number(prevBackground.slice(12,15)) * 0.85;
+        
+        let r = Number(square.getAttribute('data-r')) - Number(square.getAttribute("data-rInitial")) * 0.10;
+        let g = Number(square.getAttribute('data-g')) - Number(square.getAttribute("data-gInitial")) * 0.10;
+        let b = Number(square.getAttribute('data-b')) - Number(square.getAttribute("data-bInitial")) * 0.10;
         square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        square.setAttribute("data-r",`${r}`);
+        square.setAttribute("data-g",`${g}`);
+        square.setAttribute("data-b",`${b}`);
+        
     }
     
 }
