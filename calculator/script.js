946eb87bd8e6455ea7f66bb22b161c732349df3f
bara-934 <160777859+bara-934@users.noreply.github.com>
2024-05-14 18:43:47 +0200

    add the clear fonction to follox the operations one by one

--- calculator/script.js
+++ calculator/script.js
@@ -3,70 +3,61 @@ const multiply = (x,y) => x * y;
 const subtract = (x,y) => x - y;
 const divide = (x,y) =>(y !== 0)? x / y: display.textContent = 'NaN. Could not divide by 0';
 
-let number1;
+let number1 = undefined;
 let number2;
 let operator;
+let displayVal;
 
 function operate(x,ope,y){
     return (ope === '+') ? add(x,y)
-        : (ope === '-') ? subtract(x,y)
-        : (ope === 'x') ? multiply(x,y)
-        : (ope === '/') ? divide(x,y) : NaN;
-     
+    : (ope === '-') ? subtract(x,y)
+    : (ope === 'x') ? multiply(x,y)
+    : (ope === '/') ? divide(x,y) : NaN;
+    
 }
 
 const display = document.querySelector('.screen');
 
-document.querySelectorAll('.specials button').forEach((special)=>{
-    special.className = "specials";
-});
-
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-    button.addEventListener('click',(e)=> {
-        if(button.className !== "specials"){
-            if(display.textContent === 'Enter the operation')
-            {
-                display.textContent = button.id;    
-            }
-            else display.textContent += (button.parentNode.className === 'right')? 
-                ' ' + button.id + ' ': button.id ;
-        }
-        else{
-            if(button.id === 'clear')
-            {
-                display.textContent = 'Enter the operation';
-            }
-            else display.textContent =  `${calculate(display.textContent.split(' '))}`;
-        }
-
-    })
-});
-
-function calculate(splitted){
-    if (splitted.at(-1) === '*' || splitted.at(-1) === '/' )
-    {
-        splitted.push(1);
-    }
-    if (splitted.at(0) === '*' || splitted.at(0) === '/' )
-    {
-        splitted.unshift(1);
-    }
-    if (splitted.at(-1) === '+' || splitted.at(-1) === '-' )
-    {
-        splitted.push(0);
-    }
-    if (splitted.at(0) === '+' || splitted.at(0) === '-' )
-    {
-        splitted.unshift(0);
-    }
-
-    if(splitted.length === 3){
-        return operate(Number(splitted[0]),splitted[1],Number(splitted[2]));
-    }
-    else{
-        const copySplitted = splitted.slice(3);
-        copySplitted.unshift(calculate(splitted.slice(0,3)));
-        return calculate(copySplitted);    
-    }
+function clear()
+{
+    display.textContent = "";
 }
+
+function updateDisplay(digit)
+{
+    if(display.textContent === 'Enter the operation' || displayVal === 0) clear();
+    display.textContent += digit;
+    displayVal =  Number(display.textContent);
+}
+
+document.querySelectorAll('.digits').forEach((digit)=>{
+    digit.addEventListener('click', (e)=> {
+        updateDisplay(e.target.id);
+    })
+})
+    
+    
+document.querySelectorAll('.specials').forEach((signs)=>{
+    signs.addEventListener('click',(e)=>{
+        const button = e.target;
+        if(button.id === 'clear') {
+            display.textContent = 'Enter the operation';
+            number1 = undefined;
+            displayVal = 0;
+        }
+        else if(number1 === undefined) {
+            number1 = displayVal;
+            operator = button.id;
+            displayVal = 0;
+        }
+        else {
+            clear();
+            number2 = displayVal;
+            number1 = operate(number1, operator, number2);
+            operator = button.id;
+            updateDisplay(number1);
+            displayVal = 0;
+        }
+        
+    })
+})
