f5184bce21e95de12be11c11144fb6c15ff99401
bara-934 <160777859+bara-934@users.noreply.github.com>
2024-05-14 14:41:56 +0200

    add calculate fonction to actually display the result of any operation on the calculator

--- calculator/script.js
+++ calculator/script.js
@@ -24,21 +24,26 @@ document.querySelectorAll('.specials button').forEach((special)=>{
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener('click',(e)=> {
-        if (button.className !== "specials"){
+        if(button.className !== "specials"){
             if(display.textContent === 'Enter the operation')
             {
                 display.textContent = button.id;    
             }
-            else display.textContent += button.id;
+            else display.textContent += (button.parentNode.className === 'right')? 
+                ' ' + button.id + ' ': button.id ;
         }
         else{
             if(button.id === 'clear')
             {
                 display.textContent = 'Enter the operation';
             }
+            else display.textContent =  calculate(display.textContent);
         }
 
     })
 });
 
-
+function calculate(string){
+    arr = string.split(" ");
+    return operate(Number(arr[0]),arr[1],Number(arr[2]))
+}
