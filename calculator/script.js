16332ef5375c11444595874a6bd9e8229ff37f7b
bara-934 <160777859+bara-934@users.noreply.github.com>
2024-05-14 13:40:16 +0200

    add the event listener that updates the display screen of the calculator

--- calculator/script.js
+++ calculator/script.js
@@ -10,7 +10,35 @@ let operator;
 function operate(x,ope,y){
     return (ope === '+') ? add(x,y)
         : (ope === '-') ? subtract(x,y)
-        : (ope === '*') ? multiply(x,y)
+        : (ope === 'x') ? multiply(x,y)
         : (ope === '/') ? divide(x,y) : NaN;
      
 }
+
+const display = document.querySelector('.screen');
+
+document.querySelectorAll('.specials button').forEach((special)=>{
+    special.className = "specials";
+});
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener('click',(e)=> {
+        if (button.className !== "specials"){
+            if(display.textContent === 'Enter the operation')
+            {
+                display.textContent = button.id;    
+            }
+            else display.textContent += button.id;
+        }
+        else{
+            if(button.id === 'clear')
+            {
+                display.textContent = 'Enter the operation';
+            }
+        }
+
+    })
+});
+
+
